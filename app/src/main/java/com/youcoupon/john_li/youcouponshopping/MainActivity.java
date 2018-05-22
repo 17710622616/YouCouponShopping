164c7e3b976c2ca47150fe96512aad813d5df3cc
John <m17710622616@163.com>
2018-05-23 00:59:16 +0800

    淘宝登出

--- app/src/main/java/com/youcoupon/john_li/youcouponshopping/MainActivity.java
+++ app/src/main/java/com/youcoupon/john_li/youcouponshopping/MainActivity.java
@@ -10,17 +10,23 @@ import android.widget.Toast;
 import com.ali.auth.third.core.callback.LoginCallback;
 import com.ali.auth.third.core.model.Session;
 import com.ali.auth.third.login.LoginService;
+import com.ali.auth.third.login.callback.LogoutCallback;
+import com.alibaba.baichuan.android.trade.AlibcTradeSDK;
 import com.alibaba.baichuan.android.trade.adapter.login.AlibcLogin;
 import com.alibaba.baichuan.android.trade.callback.AlibcLoginCallback;
+import com.alibaba.baichuan.android.trade.model.AlibcShowParams;
+import com.alibaba.baichuan.android.trade.model.OpenType;
 import com.youcoupon.john_li.youcouponshopping.YouActivity.MineActivity;
 
 public class MainActivity extends AppCompatActivity {
-    private TextView doLoginTv, mineTv;
+    private TextView doLoginTv, loginOutTv, mineTv;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+
+        AlibcTradeSDK.setForceH5(false);
         doLoginTv = (TextView) findViewById(R.id.taobao_login);
         doLoginTv.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -28,11 +34,34 @@ public class MainActivity extends AppCompatActivity {
                 login();
             }
         });
+        loginOutTv = (TextView) findViewById(R.id.taobao_login_out);
+        loginOutTv.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                loginOut();
+            }
+        });
         mineTv = (TextView) findViewById(R.id.taobao_mine);
         mineTv.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 startActivity(new Intent(MainActivity.this, MineActivity.class));
+            }
+        });
+    }
+
+    private void loginOut() {
+        AlibcLogin alibcLogin = AlibcLogin.getInstance();
+
+        alibcLogin.logout(MainActivity.this, new LogoutCallback() {
+            @Override
+            public void onSuccess() {
+                Toast.makeText(MainActivity.this, "退出登录成功", Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onFailure(int code, String msg) {
+                Toast.makeText(MainActivity.this, "退出登录失败 " + code + msg, Toast.LENGTH_SHORT).show();
             }
         });
     }
--- app/src/main/java/com/youcoupon/john_li/youcouponshopping/YouUtils/YouApplication.java
+++ app/src/main/java/com/youcoupon/john_li/youcouponshopping/YouUtils/YouApplication.java
@@ -25,7 +25,7 @@ public class YouApplication extends Application {
             @Override
             public void onFailure(int code, String msg) {
                 //初始化失败，可以根据code和msg判断失败原因，详情参见错误说明
-                Toast.makeText(YouApplication.this, "阿里百川初始化失败", Toast.LENGTH_LONG).show();
+                Toast.makeText(YouApplication.this, "阿里百川初始化失败,code=" + code + ", msg=" + msg, Toast.LENGTH_LONG).show();
             }
         });
     }
